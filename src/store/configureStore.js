e0622ee9b946bccece63ae9bddeff0c5b3554a44
Ivan <ivanveselinov22@gmail.com>
2026-02-23 16:18:11 +1100

    Setup Redux devTool

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,12 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux'
 import { thunk } from 'redux-thunk';
+import { devToolsEnhancer } from 'redux-devtools-extension'; 
 import reducer from './tasks'
 
-const configureStorestore = createStore(reducer, applyMiddleware(thunk));
+const configureStorestore = createStore(
+    reducer,
+    devToolsEnhancer({trace: true}) // Enable DevTools into react + trace
+    // applyMiddleware(thunk)
+);
 
 export default configureStorestore
